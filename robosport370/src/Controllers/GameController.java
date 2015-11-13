7655200ccf1fe401b575e28e4a984677ff5589f1
landon <lzt6392@mail.usask.ca>
2015-11-12 23:48:39 -0600

    added code to game controller

--- robosport370/src/Controllers/GameController.java
+++ robosport370/src/Controllers/GameController.java
@@ -1,5 +1,64 @@
 package Controllers;
 
 public class GameController {
-
+	
+    /**
+     * All of the teams to be run in the simulation
+     */
+	Team teams[];
+	
+    /**
+     * holds the value to which team goes next
+     */
+	int nextTeamIdx;
+	
+	
+	/**
+	 * 
+	 */
+	LogContoller logger;
+	
+	/**
+	 * The size of the hexmap, the size is one side length
+	 */
+	int hexSize;
+	
+	/**
+	 * CHANGE
+	 */
+	boolean isHeadless;
+	
+	/**
+	 * Stores the type of map view
+	 */
+	MapView view;
+	
+	/**
+	 * how long it takes for each animation to complete in miliseconds
+	 */
+	int animationSpeed;
+	
+	/**
+	 * Intializes the game controller and begins the game
+	 */
+	public void init(Team teams[], int hexSize){
+	    
+	}
+	
+	public void pause(){
+	    
+	}
+	
+	public void resume(){
+	    
+	}
+	
+	public void endGame(){
+	    
+	}
+	
+	public scanSpace
+	public static void main(){
+		
+	}
 }
