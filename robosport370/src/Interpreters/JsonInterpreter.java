59f87b79f0e1bf502d30b533c48dd3db84638660
Daniel Sanche <d.sanche14@gmail.com>
2015-11-13 14:00:41 -0600

    the json interpreter can now create a robot class. I restructured the robot class to make sure all the fields lined up, and added documentation

--- robosport370/src/Interpreters/JsonInterpreter.java
+++ robosport370/src/Interpreters/JsonInterpreter.java
@@ -11,25 +11,41 @@ import java.util.HashMap;
 
 public class JsonInterpreter {
 
+    //test method
 	public static void main(String[] args) {
    JSONParser parser=new JSONParser(); 
    try {
        JSONObject json = (JSONObject) parser.parse(new FileReader("resources/RobotExample.JSON"));
        Robot newRobot = robotFromJSON(json);
+       System.out.println(newRobot);
    } catch (IOException | ParseException e1) {
        e1.printStackTrace();
    }
 	}
 
-
+/**
+ * Creates a robot object from a standard robot JSON file
+ * @param json a JSONObject representing the robot
+ * @return a robot object with the information saved in the JSON file
+ */
 static public Robot robotFromJSON(JSONObject json){
-    Robot newRobot = new Robot();
-    
-    
+      
     JSONObject root = (JSONObject) json.get("script");
     
+    //get the identification information
+    String name = (String) root.get("name");
+    String team = (String) root.get("team");
+    long serial = (long) root.get("serial");
+    
+    //get the robot's attributes
+    long health = (long) root.get("health");
+    long strength = (long) root.get("firepower");
+    long moves = (long) root.get("movement");
+    
+    //get the robot's win record
     long wins = (long) root.get("wins");
-    long kills = (long) root.get("kills");
+    long losses = (long) root.get("losses");
+    long matches = (long) root.get("matches");
     
     //find the forth code from the json
     JSONArray forth = (JSONArray) root.get("code");
@@ -53,8 +69,11 @@ static public Robot robotFromJSON(JSONObject json){
         }
     }
     
+    //create a robot from the information in the json
+    Robot newRobot = new Robot( name, team, serial, health, strength, moves, 
+                                variableList, wordList, 
+                                wins, losses, matches);
  
-    System.out.println(root);
     return newRobot;
 }
 
--- robosport370/src/Models/Robot.java
+++ robosport370/src/Models/Robot.java
@@ -1,6 +1,7 @@
 package Models;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 
 /**
@@ -11,139 +12,225 @@ import java.util.HashMap;
  * value
  */
 public class Robot {
-
-    private int health,strength,hexPosition,movesPerTurn,wins,losses,ties;
-    private String name,number;
-    HashMap forthVariables,forthCommands; //TODO: Make a hashmap
+    
+    private long serialNumber;
+    private String name, teamName;
+    private long baseHealth, currentHealth, strength, movesPerTurn, hexPosition;
+    private long wins,losses, matches; 
+    private HashMap<String,String> forthVariables,forthWords;
+    private Stack<String> mailBox;
+    
+    /**
+    this constructor will be called to create a robot. A robot can only be created if you know all of the information
+    needed for the robot to run a match
+     * 
+     * @param robotName   Name of the robot
+     * @param team        Name of the team
+     * @param serial      The robot's serial number
+     * @param health      The base health the robot starts with
+     * @param strength    The amount of damage the robot will do
+     * @param moves       The number of moves the robot can move in one turn
+     * @param vars        The list of forth variables
+     * @param words       The list of forth commands
+     * @param winCount   The number of times this robot has won a match
+     * @param lossCount  The number of times this robot has lost a match
+     * @param matchCount The total number of matches this robot has played
+     */
+    public Robot(String robotName, String team, long serial,
+            long health, long strength, long moves,
+            HashMap<String,String> vars, HashMap<String,String> words,
+            long winCount, long lossCount, long matchCount) {
+        this.name = robotName;
+        this.serialNumber = serial;
+        this.teamName = team;
+        this.forthVariables = vars;
+        this.forthWords = words;
+        this.baseHealth = health;
+        this.currentHealth = health;
+        this.strength = strength;
+        this.movesPerTurn = moves;
+        this.wins = winCount;
+        this.losses = lossCount;
+        this.matches = matchCount;
+        this.mailBox = new Stack<String>();
+    }
+    
+    /**
+     * Another constructor for if you know all of the base information for a robot, but are missing stats
+     * @param robotName   Name of the robot
+     * @param team        Name of the team
+     * @param serial      The robot's serial number
+     * @param health      The base health the robot starts with
+     * @param strength    The amount of damage the robot will do
+     * @param moves       The number of moves the robot can move in one turn
+     * @param vars        The list of forth variables
+     * @param words       The list of forth commands
+     */
+    public Robot(String robotName, String team, long serial,
+            long health, long strength, long moves,
+            HashMap<String,String> vars, HashMap<String,String> words) {
+        this(robotName, team, serial, health, strength, moves, vars, words, 0, 0, 0);
+    }
+    
+    /**
+     * @return robot's serial number
+     */
+    public long getSerialNumber(){
+        return this.serialNumber;
+    }
+    
+    /**
+     * @return robot's name
+     */
+    public String getName(){
+        return this.name;
+    }
     
     /**
      * 
-     * @return health value
+     * @return robot's team's name
      */
-    public int getHealth() {
-		return health;
-	}
+    public String getTeamName(){
+        return this.teamName;
+    }
+    
     /**
-     * 
-     * @param health
+     * @return robot's current health
      */
-	public void setHealth(int health) {
-		this.health = health;
-	}
-	/**
-	 * 
-	 * @return
-	 */
-	public int getStrength() {
-		return strength;
-	}
-	/**
-	 * 
-	 * @param strength
-	 */
-	public void setStrength(int strength) {
-		this.strength = strength;
-	}
-	/**
-	 * 
-	 * @return
-	 */
-	public int getHexPosition() {
-		return hexPosition;
-	}
-	/**
-	 * 
-	 * @param hexPosition
-	 */
-	public void setHexPosition(int hexPosition) {
-		this.hexPosition = hexPosition;
-	}
-	/**
-	 * 
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * 
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
-	 * 
-	 * @return
-	 */
-	public String getNumber() {
-		return number;
-	}
-	/**
-	 * 
-	 * @param number
-	 */
-	public void setNumber(String number) {
-		this.number = number;
-	}
+    public long getHealth() {
+        return this.currentHealth;
+	    }
+    
+    /**
+     * @return the health the robot starts a match with
+     */
+    public long getBaseHealth() {
+        return this.baseHealth;
+    }
+    
+    /**
+     * causes damage to the robot
+     * @param damage the amount to subtract from the robot's health
+     */
+	    public void inflictDamage(int damage) {
+	        this.currentHealth = this.currentHealth - damage;
+	        if(this.currentHealth <= 0){
+	            this.destroy();
+	        }
+	    }
 	
-	/**
-	 * @return the number of moves this robot can move per turn
-	 */
-	int getMovesPerTurn(){
+	    /**
+	     * @return the amount of damage the robot can cause to others
+	     */
+	    public long getStrength() {
+	        return strength;
+	    }
 	    
-	    return movesPerTurn;
-	}
-	
+	    /**
+	     * @return the number of moves the robot can do each turn
+	     */
+    long getMovesPerTurn(){
+        return movesPerTurn;
+    }
+
+    /**
+     * @return a number representing the robot's position on the board
+     */
+	    public long getPosition() {
+	        return hexPosition;
+	    }
+
+	    /**
+	     * @param newHexPosition the hex number to move this robot to
+	     */
+	    public void setPosition(long newHexPosition) {
+	        this.hexPosition = newHexPosition;
+	    }
+
+	    /**
+	     * @return the number of games this robot has won
+	     */
+	    public long getWins() {
+	       return wins;
+	    }
+	    
+	    /**
+	     * @return the number of games this robot has lost
+	     */
+	    public long getLosses() {
+	        return losses;
+	    }
+	    
+	    /**
+     * @return the total number of games this robot has played
+     */
+    public long getTotalNumberOfMatches() {
+        return this.matches;
+    }
+    
+    /**
+     * @return the value stored in the forth variable "variableName"
+     * @param variableName the name of the variable we are checking
+     */
+    public String getForthVariable(String variableName){
+        return this.forthVariables.get(variableName);
+    }
+    
+   /**
+    * Sets a new value to the saved variable passed in
+    * @param variableName the name of the variable to set
+    * @param newValue the value to set to that variable
+    */
+    public void setForthVariable(String variableName, String newValue){
+        this.forthVariables.put(variableName, newValue);
+    }
+    
+    /**
+     * gets a string representing all the logic stored in a forth word
+     * @param wordName the word we are looking for
+     * @return the forth logic stored as that word
+     */
+    public String getForthWord(String wordName){
+        return this.forthWords.get(wordName);
+    }
+    
   
-	public int getWins() {
-		return wins;
-	}
-	public int getLosses() {
-		return losses;
-	}
-	public int getTies() {
-		return ties;
-	}
-	/**
-	 * Destroys this robot
-	 */
-	void destroy()
-	{
-		this.health = 0;
-	}
+	    /**
+	     * Destroys this robot
+	     * Removes it's health, and animates it's destruction
+	     */
+	    public void destroy(){
+	        this.currentHealth = 0;
+	        //TODO: should also animate destruction, and remove the robot from the game
+	    }
 	
-	/**
-	 * @returns true if robot's health is above 0
-	 */
-	boolean isAlive()
-	{
-		if (this.health > 0){
-			return true;
-		}
-		return false;
-	}
+	    /**
+	     * @returns true if robot's health is above 0
+	     */
+	    public boolean isAlive()	{
+	        return (this.currentHealth >= 0);
+	    }
 	
-	/**
-	 * 
-	 * @param objectToPush the object that gets pushed
-	 */
-	void pushMailbox(Object objectToPush){
-
-	}
-	/**
-	 * 
-	 * @returns an object off a mailbox
-	 */
-	Object popMailbox(){
+	    /**
+	     * Saves a new value into this robot's mailbox
+	     * @param objectToPush the new value to save to the mailbox
+	     */
+	    public void pushMailbox(String objectToPush){
+	        this.mailBox.push(objectToPush);
+	    }
+	    /**
+	     * Pops a value off the robot's mailbox 
+	     * @returns the top value stored in the mailbox stack
+	     */
+	    public String popMailbox(){
+	        return this.mailBox.pop();
+	    }
 	    
-	    return null;
-	}
-
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	    /**
+	     * @return a string representation of the robot
+	     */
+	    public String toString(){
+        return name + " - " + teamName + " - " + serialNumber; 
+	    }
 
 }
