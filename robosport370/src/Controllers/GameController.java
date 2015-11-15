bbab612c9369ad8f54a42938a7dd60e89646a823
landon <lzt6392@mail.usask.ca>
2015-11-15 02:43:03 -0600

    structure in GameController.java

--- robosport370/src/Controllers/GameController.java
+++ robosport370/src/Controllers/GameController.java
@@ -1,9 +1,11 @@
 package Controllers;
 
+import Views.mapView;
+
 public class GameController {
 	
     /**
-     * All of the teams to be run in the simulation
+     * All of the teams to be run in the simulation.
      */
 	Team teams[];
 	
@@ -11,8 +13,7 @@ public class GameController {
      * holds the value to which team goes next
      */
 	int nextTeamIdx;
-	
-	
+
 	/**
 	 * 
 	 */
@@ -31,19 +32,24 @@ public class GameController {
 	/**
 	 * Stores the type of map view
 	 */
-	MapView view;
+	mapView view;
 	
 	/**
-	 * how long it takes for each animation to complete in miliseconds
+	 * how long it takes for each animation to complete in ??miliseconds??
 	 */
 	int animationSpeed;
 	
 	/**
-	 * Intializes the game controller and begins the game
+	 * initializes the teams and ??sets their position on the map??
+	 * @param allTeams an array that contains all of the teams playing the match
+	 * @param hexSize the size of the map on one side
+	 * @precond hexSize cannot be null
 	 */
-	public void init(Team teams[], int hexSize){
+	public void init(Team allTeams[], int hexSize){
+	    this.hexSize = hexSize;
 	    
 	}
+	
 	
 	public void pause(){
 	    
@@ -57,7 +63,39 @@ public class GameController {
 	    
 	}
 	
-	public scanSpace
+	public void scanSpace(int hexPos){
+	    
+	}
+	
+	/**
+	 * Gets the robot that currently has the turn to play
+	 * @param teamNum the number that represents the team that the robot
+	 * to be played is on
+	 * @param robotNum the s/n of the robot who's turn it is
+	 */
+	public void getRobot(int teamNum, int robotNum){
+	    
+	}
+	
+	public void nextTurn(){
+	    
+	}
+	
+	
+	public void moveRobot(){
+	    
+	}
+	
+	/**
+	 *  fire at the position passed in
+	 * @param shooter is the robot that will fire a shot
+	 * @param hexPos is the position that the robot is firing towards
+	 * @precond the robot must be alive
+	 * @precond the hexPos must be on the map
+	 */
+	public void shootAtSpace(Robot shooter, int hexPos){
+	    
+	}
 	public static void main(){
 		
 	}
