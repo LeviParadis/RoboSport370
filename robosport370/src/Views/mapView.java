94448fe6ee489b17b75b988675cc18f5dd9c02cd
CoreyHickson <corey.r.hickson@gmail.com>
2015-11-17 18:46:27 -0600

    Trying to get the merge to work

---
+++ robosport370/src/Views/mapView.java
@@ -0,0 +1,60 @@
+package Views;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class mapView extends ApplicationAdapter {
+    private SpriteBatch batch;
+    private Texture robots;
+    private Sprite robot;
+    
+    @Override
+    public void create() {        
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+
+    @Override
+    public void render() {   
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+	
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new mapView(), config);
+	}
+}
