3d38c1a5f980d32a67538a6f7590f9669109c0fa
Daniel Sanche <d.sanche14@gmail.com>
2015-11-25 00:12:04 -0600

    now shows a lit of teams as you set them up

--- robosport370/src/Views/setupView.java
+++ robosport370/src/Views/setupView.java
@@ -1,12 +1,15 @@
 package Views;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -16,8 +19,13 @@ import com.badlogic.gdx.graphics.g2d.ParticleEmitter.Particle;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 
 import Controllers.setupController;
+import Models.Team;
 
 /**
  * A GUI view for the setup screen
@@ -89,6 +97,8 @@ public class setupView extends ScreenAdapter {
 	private Sprite yellowPanel;
 	private Sprite greenPanel;
 	
+private Stage stage;
+private Table teamList;
 
 	private BitmapFont font = new BitmapFont(Gdx.files.internal("assets/MoonFlower.fnt"),Gdx.files.internal("assets/MoonFlower.png"),false);
 
@@ -99,6 +109,8 @@ public class setupView extends ScreenAdapter {
 	public setupView(final setupController cont) {
 		// Initailizing the controller, constants, and batch
 		controller = cont;
+
+stage = new Stage();
 		
 		SCREEN_WIDTH = Gdx.graphics.getWidth();
         SCREEN_HEIGHT = Gdx.graphics.getHeight();
@@ -131,6 +143,8 @@ public class setupView extends ScreenAdapter {
 		continueSprite.setPosition(SCREEN_WIDTH*0.2f, SCREEN_HEIGHT*0.55f);
 		returnSprite.setPosition(SCREEN_WIDTH*0.2f, SCREEN_HEIGHT*0.5f);
 		
+
+
 		// Giving the team sprites and positions
 		teamIndex = 0;
 		listener = new myTextInputListener();
@@ -186,6 +200,11 @@ public class setupView extends ScreenAdapter {
 		bluePanel.setPosition(SCREEN_WIDTH*0.18f, SCREEN_HEIGHT*0.45f);
 		yellowPanel.setPosition(SCREEN_WIDTH*0.6f, SCREEN_HEIGHT*0.48f);
 		greenPanel.setPosition(SCREEN_WIDTH*0.6f, SCREEN_HEIGHT*0.2f);
+
+this.teamList = new Table();
+teamList.setPosition(880, SCREEN_HEIGHT-285);
+teamList.setFillParent(false);
+this.stage.addActor(teamList);
     }
 	
     /**
@@ -208,6 +227,8 @@ public class setupView extends ScreenAdapter {
         font.draw(batch, "Teams", SCREEN_WIDTH*0.65f, SCREEN_HEIGHT*0.9f);
 
         batch.end();
+        
+      stage.draw();
     }   
     
     /**
@@ -249,11 +270,29 @@ public class setupView extends ScreenAdapter {
     public void addTeam() {
         
         controller.notifyAddTeam();
+        this.updateTeamTable(controller.selectedTeams);
 
     }
     
     public void deleteTeam() {
         controller.notifyDeleteTeam();
+        this.updateTeamTable(controller.selectedTeams);
+    }
+    
+    public void updateTeamTable(List<Team> teams){
+        LabelStyle labelStyle = new LabelStyle();
+        labelStyle.fontColor = Color.BLACK;
+        labelStyle.font = new BitmapFont();
+        
+        this.teamList.clear();
+        
+        Iterator<Team> it = teams.iterator();
+        while(it.hasNext()){
+            Team next = it.next();
+            Label newLabel = new Label(next.getTeamName(), labelStyle);
+            this.teamList.add(newLabel).width(50).padBottom(20);
+            this.teamList.row();
+        } 
     }
     
     public void toggleMapSize() {
@@ -356,11 +395,4 @@ public class setupView extends ScreenAdapter {
         batch.dispose();
     }
     
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "RobotSport370";
-		config.height = 800;
-		config.width = 1280;
-		new LwjglApplication(new setupController(), config);
-	}
 }
