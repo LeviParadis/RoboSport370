d67d4bfa6f5b9adf7841b7d240c4643eff3ae37b
Daniel Sanche <d.sanche14@gmail.com>
2015-12-13 15:33:09 -0600

    added constants for ui elements

---
+++ robosport370/src/Constants/UIConstants.java
@@ -0,0 +1,37 @@
+package Constants;
+
+public class UIConstants {
+        public static final String BLUE_ATLAS = "assets/ui_atlas/ui-blue.atlas";
+        public static final String RED_ATLAS = "assets/ui_atlas/ui-red.atlas";
+        public static final String GRAY_ATLAS = "assets/ui_atlas/ui-gray.atlas";
+        public static final String COMMON_ATLAS = "assets/ui_atlas/ui-commons.atlas";
+        
+        public static final String BUTTON_UP = "button_02";
+        public static final String BUTTON_DOWN = "button_01";
+        public static final String TEXTBOX = "textbox_01";
+        public static final String TEXTBOX_CURSOR = "textbox_cursor_02";
+        public static final String TEXTBOX_SELECTED = "transparent-black-30";
+        
+        public static final String BUTTON_TEXT_CANCEL = "Cancel";
+        public static final String BUTTON_TEXT_CONFIRM = "Confirm";
+        public static final String BUTTON_TEXT_ADD_ROBOT = "Add Robot";
+        public static final String BUTTON_TEXT_EDIT_ROBOT = "Edit Robot";
+        public static final String BUTTON_TEXT_SEARCH = "Search";
+        
+        public static final String SLIDER_VERTICAL = "slider_back_ver";
+        public static final String SLIDER_HORIZONTAL = "slider_back_hor";
+        
+        public static final String CHECKBOX_ON = "checkbox_on";
+        public static final String CHECKBOX_OFF = "checkbox_off";
+        
+        
+        public static final String SOUND_LASER = "assets/sound/laser.wav";
+        public static final String SOUND_EXPLOSION = "assets/sound/explosion.wav";
+        public static final String SOUND_BEEP = "assets/sound/Beep.mp3";
+        
+        public static final String SPRITE_MENU = "assets/mainMenu/menu_title.png";
+        public static final String SPRITE_CREDITS = "assets/mainMenu/menu_credits.png";
+        public static final String SPRITE_OPTIONS = "assets/mainMenu/menu_options.png";
+        
+ }
+
